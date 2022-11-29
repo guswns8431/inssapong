6301c3d93fce3659b31bc47d112f7c3dd2645ce5
오승은 <tmddms927@gmail.com>
2022-11-29 10:19:06 +0900

    delete: node_module

--- requirements/backend/start.sh
+++ requirements/backend/start.sh
@@ -1,17 +1,17 @@
 # node_package 삭제
-# if [ -e /backend/node_modules ]; then
-# 	rm -rf /backend/node_modules
-# fi
+if [ -e /backend/node_modules ]; then
+	rm -rf /backend/node_modules
+fi
 
-# if [ -e /backend/package-lock.json ]; then
-# 	rm /backend/package-lock.json
-# fi
+if [ -e /backend/package-lock.json ]; then
+	rm /backend/package-lock.json
+fi
 
-# if [ -e /backend/dist ]; then
-# 	rm -rf /backend/dist
-# fi
+if [ -e /backend/dist ]; then
+	rm -rf /backend/dist
+fi
 
 # 실행
 cd /backend
-# npm i
+npm i
 npm run start:dev
--- requirements/frontend/start.sh
+++ requirements/frontend/start.sh
@@ -1,13 +1,13 @@
 # # node_package 삭제
-# if [ -e /frontend/node_modules ]; then
-# 	rm -rf /frontend/node_modules
-# fi
+if [ -e /frontend/node_modules ]; then
+	rm -rf /frontend/node_modules
+fi
 
-# if [ -e /frontend/package-lock.json ]; then
-# 	rm /frontend/package-lock.json
-# fi
+if [ -e /frontend/package-lock.json ]; then
+	rm /frontend/package-lock.json
+fi
 
 # 실행
 cd /frontend
-# npm i
+npm i
 npm run serve
